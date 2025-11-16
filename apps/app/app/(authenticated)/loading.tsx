c5c6057c71cc99ce88f748e95a1f3be1d7a8cbf6
ROG <luan.roger.2003@gmail.com>
2025-11-16 16:18:52 -0300

    refactor: rename to AppLoading

--- apps/app/app/(authenticated)/loading.tsx
+++ apps/app/app/(authenticated)/loading.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from "@repo/design-system/components/ui/skeleton";
 
-export default function PageLoading() {
+export default function AppLoading() {
   return <Skeleton className="size-full rounded-xl" />;
 }
